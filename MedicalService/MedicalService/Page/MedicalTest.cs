26a41a09c438f309a74a4acd7ac7d086ae2f3659
QA36 <QA36@DESKTOP-QPQFVHR>
2022-11-30 19:38:15 +0100

    NewTest

---
+++ MedicalService/MedicalService/Page/MedicalTest.cs
@@ -0,0 +1,39 @@
+using MedicalService.Driver;
+
+namespace MedicalService.Page
+{
+    public class Tests
+    {
+        LoginPage loginPage;
+        MedicalPage medicalPage;
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            medicalPage = new MedicalPage();
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+        }
+
+        [Test]
+        public void TC01_MakeAppointment_ShouldAppointmentBeCompleted()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "ThisIsNotAPassword");
+            medicalPage.SelectOption("Hongkong CURA Healthcare Center");
+            medicalPage.CheckBox.Click();
+            medicalPage.MedicId.Click();
+            medicalPage.Date.SendKeys("25/12/2022");
+            medicalPage.Comment.SendKeys("Zakazano");
+            medicalPage.ButtonApp.Submit();
+
+            Assert.That("Appointment Confirmation", Is.EqualTo(medicalPage.Confirm.Text));
+        }
+    }
+}
---
+++ MedicalService/MedicalService/Tests/LoginTest.cs
@@ -0,0 +1,78 @@
+﻿using MedicalService.Driver;
+using MedicalService.Page;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalService.Tests
+{
+    public class LoginTest
+    {
+        LoginPage loginPage;
+        string Massage = "Login failed! Please ensure the username and password are valid.";
+       
+
+        [SetUp]
+        public void Setup()
+        {
+            WebDrivers.Initialize();
+            loginPage = new LoginPage();
+            
+        }
+
+        [TearDown]
+        public void Close()
+        {
+            WebDrivers.CleanUp();
+        }
+
+        [Test]
+        public void TC01_EnterInvalidUserName_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("Antonina", "ThisIsNotAPassword");
+
+            Assert.That(Massage, Is.EqualTo(loginPage.UserNotLogin.Text));
+        }
+
+        [Test]
+        public void TC02_EnterInvalidPassword_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "TestTestTest");
+
+            Assert.That(Massage, Is.EqualTo(loginPage.UserNotLogin.Text));
+        }
+
+        [Test]
+        public void TC03_EnterInvalidUserNameAndPassword_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("Antonina", "Tasic");
+
+            Assert.That(Massage, Is.EqualTo(loginPage.UserNotLogin.Text));
+        }
+
+        [Test]
+        public void TC04_EnterValidUserNameAndPassword_ShouldBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("John Doe", "ThisIsNotAPassword");
+            
+        }
+
+        [Test]
+        public void TC05_EnterNoData_ShouldNotBeLoginOnPage()
+        {
+            loginPage.AppMedic.Click();
+            loginPage.Login("", "");
+
+            Assert.That(Massage, Is.EqualTo(loginPage.UserNotLogin.Text));
+        }
+
+
+    }
+}
